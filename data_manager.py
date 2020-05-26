32811a619841ee06b64c60dc87d7203a869078d6
CocoBloom <virag.baranyai2@gmail.com>
2020-05-26 18:18:50 +0200

    update rout(/list) and add find_index()

--- data_manager.py
+++ data_manager.py
@@ -7,22 +7,27 @@ def get_list_of_questions(filename):
         return [row for row in reader]
 
 
-def get_direction(counter):
-    print(counter)
-    if counter % 2 == 0:
-        return True
+def find_index(list_of_questions,mode):
+    index = (list_of_questions[0]).index(mode)
+    if 0 <= index < 4:
+        numbers=[[int(num)for num in row[0:4]] for row in list_of_questions[1:]]
+        max_number = max(numbers, key=lambda x: x[index])
+        if int(list_of_questions[1][index]) == max_number[index]:
+            return False
+        else:
+            return True
     else:
-        return False
+        max_item= max(list_of_questions[1:], key= lambda x:x[index])
+        if list_of_questions[1][index] == max_item[index]:
+            return False
+        else:
+            return True
 
 
 def get_an_order(list_of_questions, mode, bool):
     ordered_list = ([list_of_questions[0]] + sorted(list_of_questions[1:],
                                                     key=lambda x: x[(list_of_questions[0].index(mode))], reverse=bool))
     return ordered_list
-
-
-print(get_an_order(get_list_of_questions("sample_data/question.csv"), 'id', False))
-
 
 def write_csv(filename, new_data):
     with open(filename, "w", encoding='utf-8') as file:
