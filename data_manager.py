1b1995100ffde9f3bb1d8b14ab8f6a85e56592ec
CocoBloom <virag.baranyai2@gmail.com>
2020-05-26 04:58:30 +0200

    add sort list ()

--- data_manager.py
+++ data_manager.py
@@ -1,16 +1,27 @@
 import csv
 
+
 def get_list_of_questions(filename):
     with open(filename, "r", encoding='utf-8') as file:
         reader = csv.reader(file)
         return [row for row in reader]
 
 
-print(len(get_list_of_questions("question.csv")))
-listed = get_list_of_questions("question.csv")
-revered_list=listed[::-1]
-print(listed)
-print(revered_list)
+def get_direction(counter):
+    print(counter)
+    if counter % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def get_an_order(list_of_questions, mode, bool):
+    ordered_list = ([list_of_questions[0]] + sorted(list_of_questions[1:],
+                                                    key=lambda x: x[(list_of_questions[0].index(mode))], reverse=bool))
+    return ordered_list
+
+
+print(get_an_order(get_list_of_questions("sample_data/question.csv"), 'id', False))
 
 
 def write_csv(filename, new_data):
--- server.py
+++ server.py
@@ -1,20 +1,37 @@
 from flask import Flask, request, render_template,redirect
 import data_manager
+import util
+import time
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-@app.route('/list')
+@app.route('/list', methods=['GET','POST'])
 def display_list():
-    list_of_questions = data_manager.get_list_of_questions("question.csv")
-    return render_template("list.html", list_of_questions=list_of_questions)
+    list_of_questions = data_manager.get_list_of_questions("sample_data/question.csv")
+    if request.method == "POST":
+        counters={}
+        for head in list_of_questions[0]:
+            try:
+                mode = request.form[head]
+                counter =1
+                direction = data_manager.get_direction(counter)
+                ordered_list = data_manager.get_an_order(list_of_questions=list_of_questions, mode = mode, bool=direction)
+                data_manager.write_csv("sample_data/question.csv", new_data=ordered_list)
+            except:
+                continue
+
+        return redirect('/')
+    else:
+        return render_template("list.html", list_of_questions=list_of_questions)
 
 
 
 @app.route("/question/<int:question_id>")
 def question_page(question_id):
-    list_of_questions = data_manager.get_list_of_questions('question.csv')
+    list_of_questions = data_manager.get_list_of_questions('sample_data/question.csv')
     list_of_answers = data_manager.get_list_of_questions('sample_data/answer.csv')
 
     title = list_of_questions[question_id][4]
@@ -31,7 +48,7 @@ def question_page(question_id):
 @app.route('/add-question', methods=["GET", "POST"])
 def add_question():
     if request.method == "POST":
-        list_of_questions = data_manager.get_list_of_questions("question.csv")
+        list_of_questions = data_manager.get_list_of_questions("sample_data/question.csv")
         # id-t, submission timeot honnan kap?
         new_question = [create_new_id(list_of_questions), int(time.time()), 0, 0, request.form['questiontitle'], request.form['questionbody'], '']
         list_of_questions.append(new_question)
@@ -44,12 +61,22 @@ def add_question():
 @app.route('/question/<question_id>/delete', methods=['GET', 'POST'])
 def delete_question(question_id):
     if request.method == 'POST':
-        list_of_questions = data_manager.get_list_of_questions("question.csv")
+        list_of_questions = data_manager.get_list_of_questions("sample_data/question.csv")
         for id in list_of_questions:
             if question_id == id[0]:
                 list_of_questions.remove(id)
-        data_manager.write_csv("question.csv", list_of_questions)
+        data_manager.write_csv("sample_data/question.csv", list_of_questions)
     return redirect('/list')
+
+
+@app.route("/question/<int:question_id>/new-answer", methods=['GET', 'POST'])
+def answer_page(question_id):
+    if request.method == 'POST':
+        list_of_questions = data_manager.get_list_of_questions('sample_data/question.csv')
+        list_of_questions.append([util.create_new_id(list_of_questions), int(time.time()), 0, 0,request.form['answer'], request.form['img']])
+        data_manager.write_csv("sample_data/question.csv", list_of_questions)
+        return redirect('/question/'+str(question_id))
+    return render_template("new_answer.html", question_id=question_id)
 
 
 if __name__ == "__main__":
