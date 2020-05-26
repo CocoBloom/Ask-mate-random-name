baf042f7c72bb0a46c7ecf1a77ced2c689482071
fatamorgana95 <verabertalan@gmail.com>
2020-05-26 14:47:25 +0200

    add delete_answer(), vote_up_answer(), vote_down_answer() functions

--- server.py
+++ server.py
@@ -28,7 +28,6 @@ def display_list():
         return render_template("list.html", list_of_questions=list_of_questions)
 
 
-
 @app.route("/question/<int:question_id>")
 def question_page(question_id):
     list_of_questions = data_manager.get_list_of_questions('sample_data/question.csv')
@@ -36,13 +35,13 @@ def question_page(question_id):
 
     title = list_of_questions[question_id][4]
     question = list_of_questions[question_id][5]
+    image = list_of_questions[question_id][6]
     str_id = str(question_id)
 
-    question_answers = [[answer[4],answer[5]] for answer in list_of_answers if answer[3] == str_id]
+    question_answers = [answer for answer in list_of_answers if answer[3] == str_id]
 
     return render_template("question.html", question_id= question_id, title=title,
-                           question=question, question_answers=question_answers)
-
+                           question=question, question_answers=question_answers, image=image)
 
 
 @app.route('/add-question', methods=["GET", "POST"])
@@ -51,7 +50,7 @@ def add_question():
         list_of_questions = data_manager.get_list_of_questions("sample_data/question.csv")
         new_question = [util.create_new_id(list_of_questions), int(time.time()), 0, 0, request.form['questiontitle'], request.form['questionbody'], '']
         list_of_questions.append(new_question)
-        data_manager.write_csv("questions.csv", list_of_questions)
+        data_manager.write_csv("sample_data/question.csv", list_of_questions)
         return redirect('/list')
     else:
         return render_template("ask_questions.html")
@@ -66,7 +65,6 @@ def delete_question(question_id):
                 list_of_questions.remove(id)
         data_manager.write_csv("sample_data/question.csv", list_of_questions)
     return redirect('/list')
-
 
 
 @app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
@@ -85,11 +83,44 @@ def edit_question(question_id):
 def answer_page(question_id):
     if request.method == 'POST':
         list_of_answers = data_manager.get_list_of_questions('sample_data/answer.csv')
-        list_of_answers.append([util.create_new_id(list_of_answers), int(time.time()), 0, question_id,
+        list_of_answers.append([len(list_of_answers), int(time.time()), 0, question_id,
                                   request.form['answer'], request.form['img']])
         data_manager.write_csv("sample_data/answer.csv", list_of_answers)
         return redirect('/question/'+ question_id)
     return render_template("new_answer.html", question_id=question_id)
+
+
+@app.route('/answer/<answer_id>/delete')
+def delete_answer(answer_id):
+    list_of_answers = data_manager.get_list_of_questions("sample_data/answer.csv")
+    question_id = [answer[3] for answer in list_of_answers if answer[0] == answer_id][0]
+    for answer in list_of_answers:
+        if answer_id == answer[0]:
+            list_of_answers.remove(answer)
+    data_manager.write_csv("sample_data/answer.csv", list_of_answers)
+    return redirect('/question/'+ question_id)
+
+
+@app.route('/answer/<answer_id>/vote_up')
+def vote_up_answer(answer_id):
+    list_of_answers = data_manager.get_list_of_questions("sample_data/answer.csv")
+    question_id = [answer[3] for answer in list_of_answers if answer[0] == answer_id][0]
+    for answer in list_of_answers:
+        if answer_id == answer[0]:
+            answer[2] = str(int(answer[2]) + 1)
+    data_manager.write_csv("sample_data/answer.csv", list_of_answers)
+    return redirect('/question/'+ question_id)
+
+
+@app.route('/answer/<answer_id>/vote_down')
+def vote_down_answer(answer_id):
+    list_of_answers = data_manager.get_list_of_questions("sample_data/answer.csv")
+    question_id = [answer[3] for answer in list_of_answers if answer[0] == answer_id][0]
+    for answer in list_of_answers:
+        if answer_id == answer[0]:
+            answer[2] = str(int(answer[2]) - 1)
+    data_manager.write_csv("sample_data/answer.csv", list_of_answers)
+    return redirect('/question/'+ question_id)
 
 
 if __name__ == "__main__":
